04eaf40941681b50ad135cd38dc2d124c5fcb713
omarbougarne <omarbougarne98@gmail.com>
2024-12-16 12:31:47 +0100

    Deploy

---
+++ .history/src/App_20241216100144.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
